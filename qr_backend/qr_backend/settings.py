f5b0cb3393854ba9a4637fbca040379dba71b059
Saheli Rana <138562594+SaheliRana@users.noreply.github.com>
2025-07-20 10:50:41 +0530

    Update settings.py

--- qr_backend/qr_backend/settings.py
+++ qr_backend/qr_backend/settings.py
@@ -28,7 +28,7 @@ STATICFILES_DIRS = [
 SECRET_KEY = 'django-insecure-&3(x=p0&l5(xa8k7q5na7i7uu3%(fle62&82%^_g8)2u*2q*mr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['iocl-backend.onrender.com']
 DEBUG = False
 
 
@@ -86,7 +86,9 @@ WSGI_APPLICATION = 'qr_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3'
+    )
 }
 
 # Password validation
